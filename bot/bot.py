c6d8dd2420c941ab822b0314e3c515e0b8b7636e
Rachele Margutti <rachele.margutti@studio.unibo.it>
2025-03-09 13:10:37 +0100

    Bot builder: added command handlers and bot start

---
+++ bot/bot.py
@@ -0,0 +1,15 @@
+from telegram.ext import Application
+from config.config import TOKEN
+from bot.handlers import get_handlers
+
+def main():
+    """Starts Telegram Bot"""
+    application = Application.builder().token(TOKEN).build()
+
+    # Adding bot's commands
+    for handler in get_handlers():
+        print("HANDLER: ", handler)
+        application.add_handler(handler)
+
+    print("✅ Bot started!")
+    application.run_polling()
