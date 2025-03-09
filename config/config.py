b46096e4ddb8f3e8879f64c758bb24b1589e6d3b
Rachele Margutti <rachele.margutti@studio.unibo.it>
2025-03-09 13:09:57 +0100

    Token initialization

--- config/config.py
+++ config/config.py
@@ -1,9 +1,13 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+dotenv_loaded = load_dotenv()
+print(f"✅ .env caricato: {dotenv_loaded}")
+
+print(f"📂 Percorso attuale: {os.getcwd()}")
 
 TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+print(f"🔑 Token letto: {TOKEN}")
 
 if not TOKEN:
-    raise ValueError("❌ ERRORE: Token was not found in file .env")
+    raise ValueError("❌ ERROR: Token was not found in file .env")
